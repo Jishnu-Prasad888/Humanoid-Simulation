bb47199be45c0b217888bb5e505165ecd465d77b
jishnu <thisisjishnuprasad888@gmail.com>
2026-07-03 00:26:53 +0530

    loading a humanoid model in the grid with walls

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import time
 import mujoco
 import mujoco.viewer
+import time
 
-# Load model
-model = mujoco.MjModel.from_xml_path("basic.xml")
+model = mujoco.MjModel.from_xml_path("humanoid.xml")
 
-# Runtime state
 data = mujoco.MjData(model)
 
-# Launch viewer
 with mujoco.viewer.launch_passive(model, data) as viewer:
-
     while viewer.is_running():
-
         mujoco.mj_step(model, data)
-
         viewer.sync()
-
         time.sleep(model.opt.timestep)
